b827c786795438cd6af2a7b627c49d9be93292ed
wing-puah <puahhuiying@gmail.com>
2021-08-30 17:33:26 +0800

    initial setup

---
+++ src/models/todo.js
@@ -0,0 +1,10 @@
+class Todo {
+  constructor({ id, title, description, uid }) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.uid = uid;
+  }
+}
+
+module.exports = Todo;
